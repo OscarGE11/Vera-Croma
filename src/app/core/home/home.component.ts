129bd5dccbc23b58349a35a1fad8ba8b0f3afb40
Óscar García <geoscar1100@gmail.com>
2025-06-21 15:51:43 +0200

    Added color blocks

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -4,8 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  blue = '#9483d1';
+  green = '#51b163';
+  red = '#e55e5e';
 }
