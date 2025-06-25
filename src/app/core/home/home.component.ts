dcf730bad85f88edf34e3d17d8a2dc819451d86b
Óscar García <geoscar1100@gmail.com>
2025-06-25 19:55:28 +0200

    Added copytoclipboard logic and styles, also installed angular mats and animations

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,18 +1,47 @@
 import { Component } from '@angular/core';
 import { ColorGenerationService } from '../../services/color-generation.service';
+import { Clipboard, ClipboardModule } from '@angular/cdk/clipboard';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ClipboardModule, MatTooltipModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
+  animations: [
+    trigger('fadeAnimation', [
+      state('void', style({ opacity: 0 })),
+      state('*', style({ opacity: 1 })),
+
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('250ms ease-out', style({ opacity: 1 })),
+      ]),
+
+      transition('* => void', [
+        style({ opacity: 1 }),
+        animate('250ms ease-in', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent {
-  constructor(private colorGenerationService: ColorGenerationService) {}
+  constructor(
+    private colorGenerationService: ColorGenerationService,
+    private clipboard: Clipboard
+  ) {}
 
   firstColor: string = '';
   secondColor: string = '';
   thirdColor: string = '';
+  showCopyInfo: boolean = false;
 
   ngOnInit(): void {
     this.generateColors();
@@ -34,5 +63,17 @@ export class HomeComponent {
     ) {
       this.thirdColor = this.colorGenerationService.generateColor();
     }
+  }
+
+  copyColor(color: string): void {
+    this.clipboard.copy(color);
+
+    setTimeout(() => {
+      this.showCopyInfo = true;
+    }, 100);
+
+    setTimeout(() => {
+      this.showCopyInfo = false;
+    }, 1500);
   }
 }
